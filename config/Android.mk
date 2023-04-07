1da4e7c6a96aa196edb67511f4a4ff172ce63ef2
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-04-07 22:54:25 +0200

    config: remove duplicated include
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- config/Android.mk
+++ config/Android.mk
@@ -1,8 +1,5 @@
 include $(CLEAR_VARS)
 
-# AOSP wlan directory
-include hardware/qcom/wlan/legacy/Android.mk
-
 $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_HEADERS)/*)
 	rm -rf $@
 	mkdir -p $@/include
