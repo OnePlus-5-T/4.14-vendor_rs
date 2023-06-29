39d603838720b0ad46b33942ad7af492a2d7649c
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-06-29 22:28:29 +0200

    SettingsExtra: KeyHandler: simplify things for our needs
    We don't use all these options, so KeyHandler should just change volume
    levels and vibrate.
    
    Vibrate before changing volume levels so that the feedback to the user
    is provided as soon as possible.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- packages/SettingsExtra/src/org/robertogl/settingsextra/KeyHandler.java
+++ packages/SettingsExtra/src/org/robertogl/settingsextra/KeyHandler.java
@@ -5,13 +5,9 @@
 
 package org.robertogl.settingsextra;
 
-import android.app.NotificationManager;
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.AudioSystem;
 import android.os.IBinder;
@@ -19,36 +15,18 @@ import android.os.UEventObserver;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.provider.Settings;
 import android.util.Log;
-
-import androidx.preference.PreferenceManager;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class KeyHandler extends Service {
     private AudioManager audioManager;
-    private NotificationManager notificationManager;
     private Vibrator vibrator;
-    private SharedPreferences sharedPreferences;
 
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    private boolean wasMuted = false;
-
     private static final String TriStatePath = "/sys/devices/virtual/switch/tri-state-key/state";
-
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            int stream = intent.getIntExtra(AudioManager.EXTRA_VOLUME_STREAM_TYPE, -1);
-            boolean state = intent.getBooleanExtra(AudioManager.EXTRA_STREAM_VOLUME_MUTED, false);
-            if (stream == AudioSystem.STREAM_MUSIC && !state) {
-                wasMuted = false;
-            }
-        }
-    };
 
     private final UEventObserver alertSliderEventObserver = new UEventObserver() {
         private final Object lock = new Object();
@@ -58,25 +36,8 @@ public class KeyHandler extends Service {
             synchronized (lock) {
                 String switchState = event.get("SWITCH_STATE");
                 if (switchState != null) {
+                    vibrateIfNeeded(Integer.parseInt(switchState) - 1);
                     handleMode(Integer.parseInt(switchState));
-                    return;
-                }
-                String state = event.get("STATE");
-                if (state != null) {
-                    boolean none = state.contains("USB=0");
-                    boolean vibration = state.contains("HOST=0");
-                    boolean silent = state.contains("null)=0");
-
-                    if (none && !vibration && !silent) {
-                        handleMode(POSITION_BOTTOM);
-                    } else if (!none && vibration && !silent) {
-                        Log.d(TAG, "middle");
-                        handleMode(POSITION_MIDDLE);
-                    } else if (!none && !vibration && silent) {
-                        handleMode(POSITION_TOP);
-                    }
-
-                    return;
                 }
             }
         }
@@ -85,36 +46,14 @@ public class KeyHandler extends Service {
     @Override
     public void onCreate() {
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        Context deviceProtectedContext = createDeviceProtectedStorageContext();
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(deviceProtectedContext);
 
         // Set the status at boot following the slider position
         // Do this in case the user changes the slider position while the phone is off, for example
         // Also, we solve an issue regarding the media volume that was never mute at boot
-        int tristate = Integer.parseInt(Utils.readFromFile(TriStatePath));
-        switch (tristate) {
-            case POSITION_TOP:
-                audioManager.adjustVolume(AudioManager.ADJUST_MUTE, 0);
-                audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
-                break;
-            case POSITION_MIDDLE:
-                audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0);
-                audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
-                break;
-            case POSITION_BOTTOM:
-                audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0);
-                audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
-                break;
-        }
+        handleMode(Integer.parseInt(Utils.readFromFile(TriStatePath)));
 
-        registerReceiver(
-                broadcastReceiver,
-                new IntentFilter(AudioManager.STREAM_MUTE_CHANGED_ACTION)
-        );
         alertSliderEventObserver.startObserving("tri-state-key");
-        alertSliderEventObserver.startObserving("tri_state_key");
     }
 
     @Override
@@ -140,55 +79,23 @@ public class KeyHandler extends Service {
     }
 
     private void handleMode(int position) {
-        boolean muteMedia = sharedPreferences.getBoolean(MUTE_MEDIA_WITH_SILENT, true);
-
-        int mode;
-        switch (position) {
-            case POSITION_TOP:
-                mode = Integer.parseInt(sharedPreferences.getString(ALERT_SLIDER_TOP_KEY, "0"));
-                break;
-            case POSITION_MIDDLE:
-                mode = Integer.parseInt(sharedPreferences.getString(ALERT_SLIDER_MIDDLE_KEY, "1"));
-                break;
-            case POSITION_BOTTOM:
-                mode = Integer.parseInt(sharedPreferences.getString(ALERT_SLIDER_BOTTOM_KEY, "2"));
-                break;
-            default:
-                return;
-        }
+        int mode = position - 1;
 
         executorService.submit(() -> {
         switch (mode) {
             case AudioManager.RINGER_MODE_SILENT:
                 audioManager.setRingerModeInternal(mode);
-                if (muteMedia) {
-                    audioManager.adjustVolume(AudioManager.ADJUST_MUTE, 0);
-                    wasMuted = true;
-                }
+                audioManager.adjustVolume(AudioManager.ADJUST_MUTE, 0);
                 break;
             case AudioManager.RINGER_MODE_VIBRATE:
             case AudioManager.RINGER_MODE_NORMAL:
                 audioManager.setRingerModeInternal(mode);
-                if (muteMedia && wasMuted) {
-                    audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0);
-                }
+                audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0);
                 break;
         }
-        vibrateIfNeeded(mode);
     });
     }
     private static final String TAG = "KeyHandler";
-
-    // Slider key positions
-    private static final int POSITION_TOP = 1;
-    private static final int POSITION_MIDDLE = 2;
-    private static final int POSITION_BOTTOM = 3;
-
-    // Preference keys
-    private static final String ALERT_SLIDER_TOP_KEY = "config_top_position";
-    private static final String ALERT_SLIDER_MIDDLE_KEY = "config_middle_position";
-    private static final String ALERT_SLIDER_BOTTOM_KEY = "config_bottom_position";
-    private static final String MUTE_MEDIA_WITH_SILENT = "config_mute_media";
 
     // Vibration attributes
     private static final VibrationAttributes HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES =
